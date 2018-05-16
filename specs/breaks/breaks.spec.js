21ea9df478ad8315d5e3d6bd5c0fa8f15499f52a
Fred Chasen <fchasen@gmail.com>
2018-05-16 13:16:41 -0700

    Add break test files

---
+++ specs/breaks/breaks.spec.js
@@ -0,0 +1,81 @@
+const TIMEOUT = 10000; // Some book might take longer than this to renderer
+
+describe('default', async () => {
+		let page;
+		let rendered;
+		beforeAll(async () => {
+			page = await loadPage('breaks/breaks.html')
+			return page.rendered;
+		}, TIMEOUT)
+
+		afterAll(async () => {
+			if (!DEBUG) {
+				await page.close();
+			}
+		})
+
+		it('should render 5 pages', async () => {
+			let pages = await page.$$eval(".page", (r) => {
+				return r.length;
+			});
+
+			expect(pages).toEqual(5);
+		})
+
+		it('should avoid breaking after h2', async () => {
+			let h2ParentPage = await page.$eval("h2", (r) => {
+				let pageId = r.closest(".page").id;
+				return pageId;
+			});
+			let pParentPage = await page.$eval("#afterh2", (r) => {
+				let pageId = r.closest(".page").id;
+				return pageId;
+			});
+
+			expect(h2ParentPage).toEqual(pParentPage);
+		})
+
+		it('should break after each section', async () => {
+			let sections = await page.$$eval("section", (r) => {
+				let prev, curr;
+				let section;
+				for (let i = 0; i < r.length; i++) {
+					section = r[i];
+					curr = section.closest(".page").id;
+					if(curr === prev) {
+						return false;
+					}
+					prev = curr;
+				}
+				return true;
+			});
+
+			expect(sections).toEqual(true);
+		})
+
+		it('should render a blank page before break-before=right', async () => {
+			let blank = await page.$eval(".blank_page", (r) => {
+				return r.id;
+			});
+
+			expect(blank).toEqual("page-4");
+		})
+
+		it('should render break-before=right sections as right page', async () => {
+			let isRight = await page.$eval("#page-5", (r) => {
+				return r.classList.contains("right_page");
+			});
+
+			expect(isRight).toEqual(true);
+		})
+
+		if (!DEBUG) {
+			it('should create a pdf', async () => {
+				let pdf = await page.pdf(PDF_SETTINGS);
+
+				expect(pdf).toMatchPDFSnapshot(1);
+				expect(pdf).toMatchPDFSnapshot(4);
+			})
+		}
+	}
+)

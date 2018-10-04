9c3efd96c09f3714a03d92271e3784d4a4fed70f
Fred Chasen <fchasen@gmail.com>
2018-10-04 08:57:06 -0700

    Add missing break specs

---
+++ specs/breaks/break-after/break-after-avoid/breaks-after-avoid.spec.js
@@ -0,0 +1,39 @@
+const TIMEOUT = 10000; // Some book might take longer than this to renderer
+
+describe('break-after-avoid', async () => {
+		let page;
+		let rendered;
+		beforeAll(async () => {
+			page = await loadPage('breaks/break-after/break-after-avoid/break-after-avoid.html')
+			return page.rendered;
+		}, TIMEOUT)
+
+		afterAll(async () => {
+			if (!DEBUG) {
+				await page.close();
+			}
+		})
+
+		it('should render 5 pages', async () => {
+			let pages = await page.$$eval(".pagedjs_page", (r) => {
+				return r.length;
+			});
+
+			expect(pages).toEqual(5);
+		})
+
+		it('page 3 should be Section 2', async () => {
+			let text = await page.$eval("[data-page-number='3']", (r) => r.textContent);
+
+			expect(text).toContain('Section 2');
+		})
+
+		if (!DEBUG) {
+			it('should create a pdf', async () => {
+				let pdf = await page.pdf(PDF_SETTINGS);
+
+				expect(pdf).toMatchPDFSnapshot(3);
+			})
+		}
+	}
+)

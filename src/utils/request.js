62096fdad42ce6b9ab626a9fca5757c95caf83d7
Guillaume Grossetie <ggrossetie@gmail.com>
2020-02-14 23:43:44 +0100

    Fix and enforce eslint rules
    Also fix an issue in _target-counters.js_ where `psuedo` was misspelled and undefined in the `else` clause.

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,17 +2,17 @@ export default async function request(url, options={}) {
 	return new Promise(function(resolve, reject) {
 		let request = new XMLHttpRequest();
 
-		request.open(options.method || 'get', url, true);
+		request.open(options.method || "get", url, true);
 
 		for (let i in options.headers) {
 			request.setRequestHeader(i, options.headers[i]);
 		}
 
-		request.withCredentials = options.credentials=='include';
+		request.withCredentials = options.credentials === "include";
 
 		request.onload = () => {
- 			// Chrome returns a status code of 0 for local files
- 			const status = request.status === 0 && url.startsWith('file://') ? 200 : request.status;
+			// Chrome returns a status code of 0 for local files
+			const status = request.status === 0 && url.startsWith("file://") ? 200 : request.status;
 			resolve(new Response(request.responseText, {status}));
 		};
 

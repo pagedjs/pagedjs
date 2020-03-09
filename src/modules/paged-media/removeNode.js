1ec3719f57c8487eaef35db525122076cfaa6e22
julientaq <julien@coko.foundation>
2020-03-09 08:19:13 +0100

    update to code

--- src/modules/paged-media/removeNode.js
+++ src/modules/paged-media/removeNode.js
@@ -2,20 +2,14 @@ import Handler from "../handler";
 import csstree from "css-tree";
 
 class removeNode extends Handler {
-    constructor(chunker, polisher, caller) {
-        super(chunker, polisher, caller);
-    }
+	constructor(chunker, polisher, caller) {
+		super(chunker, polisher, caller);
+	}
 
-    beforeParsed(content) {
-        // remove all script elements
-        content.querySelectorAll("script").forEach( script => { script.remove(); })
-    }
-    layoutNode(node) {
-        if(node.tagName === "script") {
-            node.remove();
-        }
-
-    }
-
+	beforeParsed(content) {
+		// remove all script elements
+		content.querySelectorAll("script").forEach( script => { script.remove(); });
+	}
 }
+
 export default removeNode;

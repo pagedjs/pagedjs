31216fdee09bcc00bd372edea9315873ae4fdcfe
Lucas Willems <lcswillems@gmail.com>
2022-08-26 10:03:19 +0200

    Export registeredHandlers

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import Chunker from "./chunker/chunker.js";
 import Polisher from "./polisher/polisher.js";
 import Previewer from "./polyfill/previewer.js";
 import Handler from "./modules/handler.js";
-import { registerHandlers, initializeHandlers } from "./utils/handlers.js";
+import {
+	registeredHandlers,
+	registerHandlers,
+	initializeHandlers
+} from "./utils/handlers.js";
 
 export {
 	Chunker,
 	Polisher,
 	Previewer,
 	Handler,
+	registeredHandlers,
 	registerHandlers,
 	initializeHandlers
 };

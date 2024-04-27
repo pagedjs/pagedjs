689be6a20b078b2be8ed56b2073b3512dd6a3b25
Nigel Cunningham <nigel@nigelcunningham.com.au>
2024-04-27 18:21:33 +1000

    Work on footnote splitting.
    Signed-off-by: Nigel Cunningham <nigel@nigelcunningham.com.au>

--- src/modules/paged-media/footnotes.js
+++ src/modules/paged-media/footnotes.js
@@ -1,5 +1,5 @@
 import Handler from "../handler.js";
-import { isContainer, isElement, isText } from "../../utils/dom.js";
+import { isContainer, isElement, isText, walk } from "../../utils/dom.js";
 import Layout from "../../chunker/layout.js";
 import csstree from "css-tree";
 
@@ -410,27 +410,77 @@ class Footnotes extends Handler {
 			let footnoteContainer = isText(startContainer) ?
 				startContainer.parentElement.closest('[data-footnote-marker]') :
 				startContainer.closest('[data-footnote-marker]');
-			let isEntireNote = (footnoteContainer &&
-				(footnoteContainer == startContainer ||
-					(footnoteContainer.childNodes[0] == startContainer && !startOffset)));
+			let notEntireNote = (!footnoteContainer || startOffset);
+			if (!notEntireNote) {
+				let pos = startContainer;
+				while (pos && pos !== footnoteContainer) {
+					pos = pos.previousSibling || pos.parentNode;
+					if (isText(pos)) {
+						notEntireNote = true;
+						break;
+					}
+				}
+			}
 
-			if (isEntireNote) {
+			if (notEntireNote) {
+				// Assuming overflow is not multipart.
+				extracted = overflow.extractContents();
+
+				let splitChild = extracted.firstElementChild;
+
+				// Add any DOM structure above this node, but remove any text
+				// content from it.
+				// Assumes the footnote content is not anything complicated enough
+				// to need the more complicated handling that we do for the main
+				// content.
+				let parentRange = document.createRange();
+				parentRange.selectNode(footnoteContainer);
+				parentRange.setEndAfter(footnoteContainer);
+				let cloned = parentRange.cloneContents();
+				let walker = walk(cloned.firstChild, cloned);
+
+				let toDelete = undefined;
+				let next, pos, replacePos, done;
+				while (!done) {
+					if (isElement(pos)) {
+						if (pos.dataset.ref == splitChild?.dataset.ref) {
+							replacePos = pos;
+						}
+
+						if (pos.dataset.footnoteMarker) {
+							// Make sure counter isn't incremented and no new marker id rendered.
+							pos.dataset.splitFrom = true;
+							delete(pos.dataset.footnoteMarker);
+						}
+					}
+					next = walker.next();
+					pos = next.value;
+					done = next.done;
+
+					if (toDelete) {
+						toDelete.remove();
+						toDelete = undefined;
+					}
+					if (isText(pos)) {
+						toDelete = pos;
+					}
+				}
+
+				if (splitChild) {
+					splitChild.dataset.splitFrom = splitChild.dataset.ref;
+				}
+
+				replacePos.parentNode.replaceChild(extracted, replacePos);
+				extracted = cloned;
+
+				this.handleAlignment(noteInnerContent.lastElementChild);
+			}
+			else {
 				// Adjust the range to take the entire footnote.
 				let range = document.createRange();
 				range.selectNode(footnoteContainer);
 				range.setEndAfter(footnoteContainer)
 				extracted = range.extractContents();
-			}
-			else {
-				// Assuming overflow is not multipart.
-				extracted = overflow.extractContents();
-
-				let splitChild = extracted.firstElementChild;
-				if (splitChild) {
-					splitChild.dataset.splitFrom = splitChild.dataset.ref;
-				}
-
-				this.handleAlignment(noteInnerContent.lastElementChild);
 			}
 
 			this.needsLayout.push(extracted);

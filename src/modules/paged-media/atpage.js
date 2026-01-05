4094be0057b14788bff754d1bb12012f344b7405
William Muir <wamuir@gmail.com>
2026-01-04 19:19:56 -0600

    Scope displayNone variable to margin box when adding marginalia

--- src/modules/paged-media/atpage.js
+++ src/modules/paged-media/atpage.js
@@ -1092,9 +1092,10 @@ class AtPage extends Handler {
 	 * @param {Object} sheet - Stylesheet to which rules are inserted.
 	 */
 	addMarginaliaContent(page, list, item, sheet) {
-		let displayNone;
 		// Just content
 		for (let loc in page.marginalia) {
+			let displayNone;
+
 			let content = csstree.clone(page.marginalia[loc]);
 			csstree.walk(content, {
 				visit: "Declaration",
